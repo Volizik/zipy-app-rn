57005a23516c8d082adc4fe77c90c99d972444a0
volizik <kizilov.yura@gmail.com>
2020-07-10 14:12:36 +0300

    fix redirect

--- src/components/WebViewCustom.tsx
+++ src/components/WebViewCustom.tsx
@@ -1,10 +1,11 @@
 import React, {FC, useRef, useState} from 'react';
-import {WebView, WebViewMessageEvent, WebViewNavigation, WebViewProps} from "react-native-webview";
+import {WebView, WebViewMessageEvent, WebViewProps} from "react-native-webview";
 import {AuthType} from "../types";
 import {facebookSignIn} from "../utils/facebook-signin";
 import {googleSignIn} from "../utils/google-signin";
 import {Loader} from "./Loader";
-import {Linking, SafeAreaView, StyleSheet} from "react-native";
+import {SafeAreaView, StyleSheet} from "react-native";
+import CookieManager from '@react-native-community/cookies';
 
 export const WebViewCustom: FC<WebViewProps> = ({children, ...props}) => {
     const webViewRef = useRef<WebView>(null);
@@ -28,20 +29,6 @@ export const WebViewCustom: FC<WebViewProps> = ({children, ...props}) => {
         }
     }
 
-    const onShouldStartLoadWithRequestHandler = ({url}: WebViewNavigation): boolean => {
-        const regExp = new RegExp('http[s]?:\/\/[a-z.]{0,4}zipy.co.il');
-
-        if (url.match(regExp) || url.includes('captcha')) {
-            console.log(url);
-            return true;
-        }
-        Linking.canOpenURL(url)
-            .then(() => Linking.openURL(url))
-            .catch(() => console.log(`Don't know how to open this URL: ${url}`))
-
-        return false;
-    }
-
     const injectedJavascript = `
             document.documentElement.classList.add('zipy-mobile-app');
     `;
@@ -55,7 +42,6 @@ export const WebViewCustom: FC<WebViewProps> = ({children, ...props}) => {
                 injectedJavaScript={injectedJavascript}
                 onLoadStart={() => setIsLoading(true)}
                 onLoadEnd={() => setIsLoading(false)}
-                onShouldStartLoadWithRequest={onShouldStartLoadWithRequestHandler}
                 {...props}
             />
             {isLoading && (
