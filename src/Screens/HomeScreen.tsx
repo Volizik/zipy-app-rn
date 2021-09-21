bdf83cdc1cdf132d455dde95be55f46b667d6b8e
Volizik <kizilov.yura@gmail.com>
2021-09-21 15:17:49 +0300

    fixed useragent

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { HOME_URL } from 'react-native-dotenv';
 
 import {WebViewCustom} from "../components/WebViewCustom";
+import { useParamsToUrl } from '../hooks/useParamsToUrl'
 
 export const HomeScreen = () => {
+    const { addParamsToUrl } = useParamsToUrl();
 
     return (
         <WebViewCustom
-            source={{ uri: HOME_URL }}
+            source={{ uri: addParamsToUrl(HOME_URL) }}
         />
     );
 };
--- src/components/WebViewCustom.tsx
+++ src/components/WebViewCustom.tsx
@@ -1,12 +1,12 @@
-import React, {FC, memo, useRef, useState, forwardRef} from 'react';
+import React, {FC} from 'react';
 import {SafeAreaView, StyleSheet} from "react-native";
-import {WebView, WebViewNavigation, WebViewProps } from 'react-native-webview';
-import { WebViewNavigationEvent } from 'react-native-webview/lib/WebViewTypes';
+import {WebView, WebViewProps } from 'react-native-webview';
+// import { WebViewNavigationEvent } from 'react-native-webview/lib/WebViewTypes';
 import UserAgent from 'react-native-user-agent';
-import { HOME_URL_DOMAIN } from 'react-native-dotenv';
+// import { HOME_URL_DOMAIN } from 'react-native-dotenv';
 
 import { Loader } from "./Loader";
-import { useParamsToUrl } from '../hooks/useParamsToUrl'
+// import { useParamsToUrl } from '../hooks/useParamsToUrl'
 
 interface WebViewCustomProps extends Omit<WebViewProps, 'source'> {
     source: {
@@ -18,42 +18,42 @@ const injectedJavascript = `
 document.documentElement.classList.add('zipy-mobile-app');
 `;
 
-export const WebViewCustom: FC<WebViewCustomProps> = ({children, source, ...props}) => {
-    const webViewRef = useRef<WebView>(null);
-    const { addParamsToUrl, hasVersionParam, hasUtmParam } = useParamsToUrl();
-    const [currentUrl, setCurrentUrl] = useState<string>(addParamsToUrl(source.uri));
+export const WebViewCustom: FC<WebViewCustomProps> = ({children, ...props}) => {
+    // const webViewRef = useRef<WebView>(null);
+    // const { addParamsToUrl, hasVersionParam, hasUtmParam } = useParamsToUrl();
+    // const [currentUrl, setCurrentUrl] = useState<string>(addParamsToUrl(source.uri));
 
-    const onLoadStartHandler = ({nativeEvent: { url }}: WebViewNavigationEvent) => {
-        if (!hasVersionParam(url) || !hasUtmParam(url)) {
-            webViewRef.current?.stopLoading();
+    // const onLoadStartHandler = ({nativeEvent: { url }}: WebViewNavigationEvent) => {
+    //     if (!hasVersionParam(url) || !hasUtmParam(url)) {
+    //         webViewRef.current?.stopLoading();
 
-            const urlWithParams = addParamsToUrl(url);
-            if (url.split('?')[0].includes(HOME_URL_DOMAIN) && urlWithParams !== currentUrl) {
-                setCurrentUrl(urlWithParams);
-            }
-        }
-    }
+    //         const urlWithParams = addParamsToUrl(url);
+    //         if (url.split('?')[0].includes(HOME_URL_DOMAIN) && urlWithParams !== currentUrl) {
+    //             setCurrentUrl(urlWithParams);
+    //         }
+    //     }
+    // }
 
-    const onShouldStartLoadWithRequest = ({ url }: WebViewNavigation) => {
-        // reject zipy.co.il link if you are on this zipy.co.il, to escape redirect to v2
-        return currentUrl.split('?')[0] !== url
-    }
+    // const onShouldStartLoadWithRequest = ({ url }: WebViewNavigation) => {
+    //     // reject zipy.co.il link if you are on this zipy.co.il, to escape redirect to v2
+    //     return currentUrl.split('?')[0] !== url
+    // }
 
     return (
         <SafeAreaView style={styles.wrapper}>
             <WebView
                 style={styles.webView}
-                ref={webViewRef}
+                // ref={webViewRef}
                 allowsBackForwardNavigationGestures={true}
                 onMessage={() => {}}
                 injectedJavaScript={injectedJavascript}
-                userAgent={UserAgent.getUserAgent()}
+                userAgent={UserAgent.getUserAgent().replace('ZipyRN', 'ZipyAppIOS')}
                 startInLoadingState={true}
                 renderLoading={() => <Loader />}
                 mixedContentMode="compatibility"
-                onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
-                onLoadStart={onLoadStartHandler}
-                source={{ uri: currentUrl }}
+                // onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
+                // onLoadStart={onLoadStartHandler}
+                // source={{ uri: currentUrl }}
                 {...props}
             />
         </SafeAreaView>
