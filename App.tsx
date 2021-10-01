a7032cbecea38b6475503b660b03b5505773a70f
Volizik <kizilov.yura@gmail.com>
2021-10-01 14:48:23 +0300

    added request for asking tracking permission

--- App.tsx
+++ App.tsx
@@ -1,73 +1,35 @@
-// import 'react-native-gesture-handler';
-import React, {FC} from 'react';
-// import {Platform} from "react-native";
-// import {NavigationContainer} from "@react-navigation/native";
-// import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-// import {SvgProps} from "react-native-svg";
+import React from 'react';
+import { Alert } from 'react-native';
+import {
+    getTrackingStatus,
+    requestTrackingPermission,
+  } from 'react-native-tracking-transparency';
 
 import {HomeScreen} from "./src/Screens/HomeScreen";
-// import {CurrencyScreen} from './src/Screens/CurrencyScreen';
-// import {TrackingScreen} from './src/Screens/TrackingScreen';
-// import {SizeChartScreen} from './src/Screens/SizeChartScreen';
-// import {FindzipScreen} from './src/Screens/FindzipScreen';
-// import {ZipyIcon} from './src/components/icons/ZipyIcon';
-// import {TrackingIcon} from './src/components/icons/TrackingIcon';
-// import {SizeChartIcon} from './src/components/icons/SizeChartIcon';
-// import {ZipCodeIcon} from './src/components/icons/ZipCodeIcon';
-// import {CalculatorIcon} from './src/components/icons/CalculatorIcon';
-// import { tabsNames } from './src/constants'
 
 declare const global: {HermesInternal: null | {}};
 
-// const Tab = createBottomTabNavigator();
-
 const App = () => {
 
-  return (
-      <>
-          {/* {Platform.OS === 'ios' ? (
-              <NavigationContainer>
-                  <Tab.Navigator
-                      initialRouteName={tabsNames.home}
-                      screenOptions={({ route, navigation }) => ({
-                          tabBarIcon: ({ color, size, focused }) => {
-                              let icons: {[key: string]: FC<SvgProps>} = {
-                                  [tabsNames.tracking]: TrackingIcon,
-                                  [tabsNames.sizeChart]: SizeChartIcon,
-                                  [tabsNames.home]: ZipyIcon,
-                                  [tabsNames.zipCode]: ZipCodeIcon,
-                                  [tabsNames.calculator]: CalculatorIcon,
-                              };
-                              const DynamicIcon = icons[route.name];
+  React.useEffect(() => {
+    getTrackingStatus()
+      .then((status) => {
+          if (status === 'not-determined') {
+            request();
+          }
+      })
+      .catch((e) => Alert.alert('Error', e?.toString?.() ?? e));
+  }, []);
 
-                              return <DynamicIcon
-                                        color={color} 
-                                        width={size} 
-                                        height={size} 
-                                    />;
-                          },
+  const request = React.useCallback(async () => {
+    try {
+      await requestTrackingPermission();
+    } catch (e) {
+      Alert.alert('Error', e?.toString?.() ?? e);
+    }
+  }, []);
 
-                      })}
-                      tabBarOptions={{
-                          activeTintColor: '#7866ff',
-                          inactiveTintColor: '#8D8D8F',
-                          labelStyle: {
-                              fontSize: 12
-                          },
-                      }}
-
-                  >
-                      <Tab.Screen name={tabsNames.tracking} component={TrackingScreen} />
-                      <Tab.Screen name={tabsNames.sizeChart} component={SizeChartScreen} />
-                      <Tab.Screen name={tabsNames.home} component={HomeScreen} />
-                      <Tab.Screen name={tabsNames.zipCode} component={FindzipScreen} />
-                      <Tab.Screen name={tabsNames.calculator} component={CurrencyScreen} />
-                  </Tab.Navigator>
-              </NavigationContainer> */}
-          {/* ) : <HomeScreen/>} */}
-          <HomeScreen/>
-      </>
-  )
+  return <HomeScreen/>;
 };
 
 export default App;
--- src/components/WebViewCustom.tsx
+++ src/components/WebViewCustom.tsx
@@ -1,12 +1,9 @@
 import React, {FC} from 'react';
 import {SafeAreaView, StyleSheet} from "react-native";
 import {WebView, WebViewProps } from 'react-native-webview';
-// import { WebViewNavigationEvent } from 'react-native-webview/lib/WebViewTypes';
 import UserAgent from 'react-native-user-agent';
-// import { HOME_URL_DOMAIN } from 'react-native-dotenv';
 
 import { Loader } from "./Loader";
-// import { useParamsToUrl } from '../hooks/useParamsToUrl'
 
 interface WebViewCustomProps extends Omit<WebViewProps, 'source'> {
     source: {
@@ -19,31 +16,11 @@ document.documentElement.classList.add('zipy-mobile-app');
 `;
 
 export const WebViewCustom: FC<WebViewCustomProps> = ({children, ...props}) => {
-    // const webViewRef = useRef<WebView>(null);
-    // const { addParamsToUrl, hasVersionParam, hasUtmParam } = useParamsToUrl();
-    // const [currentUrl, setCurrentUrl] = useState<string>(addParamsToUrl(source.uri));
-
-    // const onLoadStartHandler = ({nativeEvent: { url }}: WebViewNavigationEvent) => {
-    //     if (!hasVersionParam(url) || !hasUtmParam(url)) {
-    //         webViewRef.current?.stopLoading();
-
-    //         const urlWithParams = addParamsToUrl(url);
-    //         if (url.split('?')[0].includes(HOME_URL_DOMAIN) && urlWithParams !== currentUrl) {
-    //             setCurrentUrl(urlWithParams);
-    //         }
-    //     }
-    // }
-
-    // const onShouldStartLoadWithRequest = ({ url }: WebViewNavigation) => {
-    //     // reject zipy.co.il link if you are on this zipy.co.il, to escape redirect to v2
-    //     return currentUrl.split('?')[0] !== url
-    // }
 
     return (
         <SafeAreaView style={styles.wrapper}>
             <WebView
                 style={styles.webView}
-                // ref={webViewRef}
                 allowsBackForwardNavigationGestures={true}
                 onMessage={() => {}}
                 injectedJavaScript={injectedJavascript}
@@ -51,9 +28,7 @@ export const WebViewCustom: FC<WebViewCustomProps> = ({children, ...props}) => {
                 startInLoadingState={true}
                 renderLoading={() => <Loader />}
                 mixedContentMode="compatibility"
-                // onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
-                // onLoadStart={onLoadStartHandler}
-                // source={{ uri: currentUrl }}
+                originWhitelist={['https://', 'file://']}
                 {...props}
             />
         </SafeAreaView>
